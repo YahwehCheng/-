4e18ce5753d8bc9251d22eb931e752785afda363
YahwehCheng <quietsquall@icloud.com>
2023-09-24 15:46:57 +0800

    实验一

---
+++ OutputDemo.java
@@ -0,0 +1,22 @@
+public class OutputDemo {
+
+    public static void main(String[] args) {
+        int x,y;
+        int count=0;
+        for(x=2;x<=2000;x++) {
+            boolean flag=true;
+            for(y=2;y<x;y++) {
+                if(x%y==0) {
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag) {
+                count++;
+                System.out.print(y+"\t");
+                if(count%5==0)
+                    System.out.println();
+            }
+        }
+    }
+}
